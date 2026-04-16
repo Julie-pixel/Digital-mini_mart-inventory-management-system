3122c2157ecaaa69947d898f206630385f5120c0
Julie-pixel <julie8918@gmail.com>
2026-04-16 20:31:21 +0300

    db.php

--- config/db.php
+++ config/db.php
@@ -1,9 +1,11 @@
 <?php
-$host = "db";
+$host = "localhost";  // FIXED
 $user = "root";
 $password = "";
 $database = "digital_mini_mart_inventory_db";
+
 $conn = mysqli_connect($host, $user, $password, $database);
+
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }

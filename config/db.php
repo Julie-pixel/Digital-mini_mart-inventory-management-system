c1c4301fd65ff3a708a4e29930fa94ac93fccf4f
Julie-pixel <julie8918@gmail.com>
2026-04-16 20:35:21 +0300

    db.phps

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";  // FIXED
+$host = "db";
 $user = "root";
-$password = "";
+$password = "rootpassword";
 $database = "digital_mini_mart_inventory_db";
 
 $conn = mysqli_connect($host, $user, $password, $database);
